ffd39ef0fcd6972bd149f4f92c826c13ac4f970a
aastankeev <aastankeev@outlook.com>
2024-12-17 09:53:05 +0800

    Update BL.user.js

--- BL.user.js
+++ BL.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Bums launcher
 // @namespace    Violentmonkey Scripts
-// @version      6
-// @description  30 сек / додумался как сделать тока для бомжа
+// @version      7
+// @description  
 // @match        *://*web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3Dbums%26appname%3Dapp%26startapp%3Dref_kRO2nQAi*
+// @match        *://*web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3DplanesCryptobot%26appname%3Dplanes%26startapp%3Dref_76H4RJ*
 // @grant        none
 // @icon         https://app.bums.bot/favicon.ico
 // @downloadURL https://github.com/aastankeev/simple/raw/main/BL.user.js
