a828a2b614f4ff03667dd1c519bd049c7bcd5e33
aastankeev <aastankeev@outlook.com>
2025-01-02 18:45:44 +0800

    Update tac.user.js

--- tac.user.js
+++ tac.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         telegram apps center
 // @namespace    http://tampermonkey.net/
-// @version      3
+// @version      4
 // @description
-// @author       
+// @author
 // @match        *://*tappscenter.org/*
 // @grant        none
 // @icon         https://tappscenter.org/favicon.ico
@@ -18,14 +18,24 @@
     // Флаг для отслеживания появления элемента
     let elementFound = false;
 
-    // Функция для проверки наличия элемента каждые 5 секунд
+    // Функция для проверки наличия элемента каждые 1 секунду
     const interval = setInterval(() => {
-        const element = document.querySelector('.styles_tapHighLight__adwkg.StreaksBanner_panelInner__rqKQg.styles_rippleContainer__y7gWh');
+        const element = document.querySelector('.StreaksBanner_root__k2yVY .StreaksBanner_panel__iNkBw .styles_tapHighLight__adwkg.StreaksBanner_panelInner__rqKQg.styles_rippleContainer__y7gWh');
         if (element) {
             element.click(); // Клик по элементу
             elementFound = true;
             clearInterval(interval); // Остановка проверки
             console.log('Элемент найден и клик выполнен.');
+        } else {
+            console.log('Элемент не найден, повторная проверка...');
         }
     }, 1000);
+
+    // Опционально: остановить таймер через определенное время, если элемент не найден
+    setTimeout(() => {
+        if (!elementFound) {
+            clearInterval(interval);
+            console.log('Элемент не найден за заданное время.');
+        }
+    }, 60000); // 60 секунд на поиск
 })();
