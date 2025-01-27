3a2b0948884ba458918f6113cfb89c51b5d6bbf1
aastankeev <aastankeev@outlook.com>
2025-01-27 12:23:16 +0800

    Create fp.user.js

---
+++ fp.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         PocketFI
+// @namespace    Violentmonkey Scripts
+// @version      1
+// @description  
+// @match        *://*pocketfi.app/*
+// @grant        none
+// @icon         https://pocketfi.app/vite.svg
+// @downloadURL  https://github.com/aastankeev/simple/raw/main/fp.user.js
+// @updateURL    https://github.com/aastankeev/simple/raw/main/fp.user.js
+// @homepage     https://github.com/aastankeev/simple
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Флаг для отслеживания нажатия
+    let elementClicked = false;
+
+    // Функция поиска и нажатия на элемент
+    function findAndClickElement() {
+        // Ищем элемент по селектору
+        const element = document.querySelector("span.font-extrabold.leading-tight.text-md.select-none");
+
+        if (element && !elementClicked) {
+            element.click(); // Нажимаем на элемент
+            elementClicked = true; // Устанавливаем флаг
+            console.log("Элемент найден и нажат. Скрипт завершен.");
+        } else {
+            console.log("Элемент не найден.");
+        }
+    }
+
+    // Выполняем поиск элемента
+    findAndClickElement();
+
+    // Проверяем флаг, чтобы завершить скрипт
+    if (elementClicked) {
+        return;
+    } else {
+        // Если элемент не найден сразу, пытаемся повторить через небольшую задержку
+        setTimeout(() => {
+            findAndClickElement();
+        }, 3000);
+    }
+})();
