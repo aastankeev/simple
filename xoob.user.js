900cf810f27ef637604dbcd02c2c2b6bc3ee2cf2
aastankeev <aastankeev@outlook.com>
2025-03-10 11:17:16 +0800

    Update xoob.user.js

--- xoob.user.js
+++ xoob.user.js
@@ -1,7 +1,7 @@
-// ==UserScript==
+// ==UserScript== 
 // @name         xoob
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      2
 // @description
 // @author
 // @match        *://*game.xoob.gg*/
@@ -15,32 +15,34 @@
 (function() {
     'use strict';
 
-    function waitForElement(selector, callback, interval = 500) {
-        const observer = new MutationObserver(() => {
-            const element = document.querySelector(selector);
-            if (element) {
-                observer.disconnect();
-                callback(element);
-            }
-        });
+    setTimeout(() => {
+        function waitForElement(selector, callback, interval = 500) {
+            const observer = new MutationObserver(() => {
+                const element = document.querySelector(selector);
+                if (element) {
+                    observer.disconnect();
+                    callback(element);
+                }
+            });
 
-        observer.observe(document.body, { childList: true, subtree: true });
+            observer.observe(document.body, { childList: true, subtree: true });
 
-        // На случай, если кнопка уже есть
-        setTimeout(() => {
-            const element = document.querySelector(selector);
-            if (element) {
-                observer.disconnect();
-                callback(element);
-            }
-        }, interval);
-    }
-
-    waitForElement(
-        "div.flex.w-1\\/2.items-center.justify-center.px-2.font-futura-medium.text-center.font-medium.text-md.h-\\[48px\\].bg-heliotrope.rounded-\\[32px\\].shadow-4.text-\\[\\#000024\\]",
-        (button) => {
-            console.log("Кнопка найдена, нажимаю...");
-            button.click();
+            // На случай, если кнопка уже есть
+            setTimeout(() => {
+                const element = document.querySelector(selector);
+                if (element) {
+                    observer.disconnect();
+                    callback(element);
+                }
+            }, interval);
         }
-    );
+
+        waitForElement(
+            "div.flex.w-1\/2.items-center.justify-center.px-2.font-futura-medium.text-center.font-medium.text-md.h-\[48px\].bg-heliotrope.rounded-\[32px\].shadow-4.text-\[\#000024\]",
+            (button) => {
+                console.log("Кнопка найдена, нажимаю...");
+                button.click();
+            }
+        );
+    }, 10000); // Задержка 10 секунд перед запуском
 })();
