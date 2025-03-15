fd81be28f4dda1eb36fb4c2509e0c0f501f31040
aastankeev <aastankeev@outlook.com>
2025-03-15 13:06:47 +0800

    Update b.user.js

--- b.user.js
+++ b.user.js
@@ -1,7 +1,7 @@
-// ==UserScript==
+// ==UserScript== 
 // @name         Bums
 // @namespace    Violentmonkey Scripts
-// @version      28
+// @version      29
 // @description  45 дней до дропа
 // @match        *://*app.bums.bot/*
 // @grant        none
@@ -11,86 +11,78 @@
 // @homepage     https://github.com/aastankeev/simple
 // ==/UserScript==
 
-
 (function () {
     'use strict';
 
-function waitForNiceButton() {
-    return new Promise(resolve => {
-        let attempts = 0;
+    function monitorNiceButton() {
         const interval = setInterval(() => {
             let niceButton = document.querySelector('.van-button--success span.van-button__text');
             if (niceButton && niceButton.textContent.includes('Nice')) {
                 niceButton.click();
                 clearInterval(interval);
-                setTimeout(resolve, 1000);
-            } else if (attempts >= 5) {
-                clearInterval(interval);
-                resolve();
             }
-            attempts++;
         }, 1000);
-    });
-}
-
-function waitForAirdropRules() {
-    const checkInterval = setInterval(() => {
-        let airdropButton = [...document.querySelectorAll('.icon')].find(button => {
-            let textElement = button.querySelector('.name .cut_two');
-            return textElement && textElement.innerHTML.includes('AIRDROP<br>RULES');
-        });
-
-        if (airdropButton) {
-            clearInterval(checkInterval);
-            console.log("Кнопка 'AIRDROP RULES' найдена. Запускаем скрипт...");
-            processAirdropRules(airdropButton);
-        }
-    }, 1000);
-}
-
-function processAirdropRules(button) {
-    button.click();
-    setTimeout(() => {
-        let dailyLogin = [...document.querySelectorAll('.left_text')].find(el => {
-            let textEl = el.querySelector('.text_bold');
-            return textEl && textEl.textContent.includes('DAILY LOG-IN');
-        });
-
-        if (dailyLogin) {
-            dailyLogin.click();
-            setTimeout(() => {
-                let getFreeBtn = document.querySelector('.getFree');
-                if (getFreeBtn) {
-                    getFreeBtn.click();
-                    setTimeout(closePopups, 1000);
-                } else {
-                    closePopups();
-                }
-            }, 1000);
-        } else {
-            startMainScript();
-        }
-    }, 1000);
-}
-
-function closePopups() {
-    let overlayPopup = document.querySelector('div.van-overlay.van-fade-leave-from.van-fade-leave-active');
-    if (overlayPopup) {
-        overlayPopup.click();
     }
-    setTimeout(() => {
-        let backButton = document.querySelector('div#app > a.back-button > img');
-        if (backButton) {
-            backButton.click();
+
+    function waitForAirdropRules() {
+        const checkInterval = setInterval(() => {
+            let airdropButton = [...document.querySelectorAll('.icon')].find(button => {
+                let textElement = button.querySelector('.name .cut_two');
+                return textElement && textElement.innerHTML.includes('AIRDROP<br>RULES');
+            });
+
+            if (airdropButton) {
+                clearInterval(checkInterval);
+                console.log("Кнопка 'AIRDROP RULES' найдена. Запускаем скрипт...");
+                processAirdropRules(airdropButton);
+            }
+        }, 1000);
+    }
+
+    function processAirdropRules(button) {
+        button.click();
+        setTimeout(() => {
+            let dailyLogin = [...document.querySelectorAll('.left_text')].find(el => {
+                let textEl = el.querySelector('.text_bold');
+                return textEl && textEl.textContent.includes('DAILY LOG-IN');
+            });
+
+            if (dailyLogin) {
+                dailyLogin.click();
+                setTimeout(() => {
+                    let getFreeBtn = document.querySelector('.getFree');
+                    if (getFreeBtn) {
+                        getFreeBtn.click();
+                        setTimeout(closePopups, 1000);
+                    } else {
+                        closePopups();
+                    }
+                }, 1000);
+            } else {
+                startMainScript();
+            }
+        }, 1000);
+    }
+
+    function closePopups() {
+        let overlayPopup = document.querySelector('div.van-overlay.van-fade-leave-from.van-fade-leave-active');
+        if (overlayPopup) {
+            overlayPopup.click();
         }
-        setTimeout(startMainScript, 1000);
-    }, 1000);
-}
+        setTimeout(() => {
+            let backButton = document.querySelector('div#app > a.back-button > img');
+            if (backButton) {
+                backButton.click();
+            }
+            setTimeout(startMainScript, 1000);
+        }, 1000);
+    }
 
-function startMainScript() {
-    console.log("Основной скрипт запущен...");
-    // Здесь код основного скрипта
-}
+    function startMainScript() {
+        console.log("Основной скрипт запущен...");
+        // Здесь код основного скрипта
+    }
 
-waitForNiceButton().then(waitForAirdropRules);
+    monitorNiceButton();
+    waitForAirdropRules();
 })();
