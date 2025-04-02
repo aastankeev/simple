a95c2fbbb12e881afed37427129a487362bbfdab
aastankeev <aastankeev@outlook.com>
2025-04-02 16:41:07 +0800

    Update BL.user.js

--- BL.user.js
+++ BL.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bums launcher
 // @namespace    Violentmonkey Scripts
-// @version      12
+// @version      13
 // @description  
 // @match        *://*web.telegram.org/a/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3Dbums%26appname%3Dapp%26startapp%3Dref_kRO2nQAi*
 // @match        *://*web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3Dbums%26appname%3Dapp%26startapp%3Dref_kRO2nQAi*
@@ -9,7 +9,7 @@
 // @match        *://*web.telegram.org/a/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3Dzoo_story_bot%26appname%3Dgame%26startapp%3Dref707981986*
 // @match        *://*web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3Dzoo_story_bot%26appname%3Dgame%26startapp%3Dref707981986*
 // @match        *://*web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3D@theplanetonbot%26appname%3Dgame%26startapp%3D*
-// @match        *://*cdn.thetreasury.io/*
+// @match        *://*web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3DPlanetX_gamebot%26appname%3Dgame%26startapp%3D707981986*
 // @grant        none
 // @icon         https://app.bums.bot/favicon.ico
 // @downloadURL https://github.com/aastankeev/simple/raw/main/BL.user.js
